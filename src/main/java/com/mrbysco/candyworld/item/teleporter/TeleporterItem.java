e93f0b0f7fbb1abce070d43fc9e185bf0215ec71
Mrbysco <Mrbysco@users.noreply.github.com>
2023-02-20 20:27:43 +0100

    Adjust how disableTeleporter is setup

--- src/main/java/com/mrbysco/candyworld/item/teleporter/TeleporterItem.java
+++ src/main/java/com/mrbysco/candyworld/item/teleporter/TeleporterItem.java
@@ -45,41 +45,45 @@ public class TeleporterItem extends Item {
 	@Override
 	@ParametersAreNonnullByDefault
 	public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity entity) {
+		if (CandyConfig.COMMON.disableTeleporter.get()) {
+			return stack;
+		}
 		if (stack.isEdible()) {
 			world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), entity.getEatingSound(stack), SoundSource.NEUTRAL, 1.0F, 1.0F + (world.random.nextFloat() - world.random.nextFloat()) * 0.4F);
 			entity.addEatEffect(stack, world, entity);
 
 			if (entity instanceof Player player) {
 				player.awardStat(Stats.ITEM_USED.get(stack.getItem()));
-				if (player instanceof ServerPlayer && (CandyConfig.COMMON.disableTeleporter.get() || world.dimension() == getDimension())) {
+				if (player instanceof ServerPlayer && (world.dimension() == getDimension())) {
 					CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayer) player, stack);
 				}
 			}
 
-			if (!(entity instanceof Player) || (world.dimension() == getDimension() && !((Player) entity).getAbilities().instabuild) || CandyConfig.COMMON.disableTeleporter.get()) {
+			if (!(entity instanceof Player) || (world.dimension() == getDimension() && !((Player) entity).getAbilities().instabuild)) {
 				stack.shrink(1);
 			}
-		}
-		if (!world.isClientSide && entity instanceof Player && !CandyConfig.COMMON.disableTeleporter.get()) {
-			ServerPlayer player = (ServerPlayer) entity;
 
-			if (world.dimension() == getDimension()) {
-				if (!ForgeHooks.onTravelToDimension(player, Level.OVERWORLD))
-					return stack;
+			if (!world.isClientSide && entity instanceof Player) {
+				ServerPlayer player = (ServerPlayer) entity;
 
-				teleportToDimension(world, player, Level.OVERWORLD);
-			} else {
-				if (!ForgeHooks.onTravelToDimension(player, getDimension()))
-					return stack;
+				if (world.dimension() == getDimension()) {
+					if (!ForgeHooks.onTravelToDimension(player, Level.OVERWORLD))
+						return stack;
 
-				teleportToDimension(world, player, getDimension());
+					teleportToDimension(world, player, Level.OVERWORLD);
+				} else {
+					if (!ForgeHooks.onTravelToDimension(player, getDimension()))
+						return stack;
+
+					teleportToDimension(world, player, getDimension());
+				}
 			}
 		}
 		return stack;
 	}
 
 	public void teleportToDimension(Level worldIn, Player player, ResourceKey<Level> dimension) {
-		if (player.isAlive() && !worldIn.isClientSide()) {
+		if (player.isAlive() && !worldIn.isClientSide() && !CandyConfig.COMMON.disableTeleporter.get()) {
 			if (!player.isPassenger() && !player.isVehicle() && player.canChangeDimensions()) {
 				ServerPlayer serverPlayer = (ServerPlayer) player;
 				MinecraftServer server = player.getServer();
